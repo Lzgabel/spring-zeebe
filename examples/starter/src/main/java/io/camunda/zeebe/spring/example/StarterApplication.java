b6eb117c66af967c75462edc12fe7992a136d215
李銍 <lizhi@shuwen.com>
2021-06-11 10:09:54 +0800

    feat(spring-zeebe): support configure resources with classpath:*

--- examples/starter/src/main/java/io/camunda/zeebe/spring/example/StarterApplication.java
+++ examples/starter/src/main/java/io/camunda/zeebe/spring/example/StarterApplication.java
@@ -15,7 +15,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 @SpringBootApplication
 @EnableZeebeClient
 @EnableScheduling
-@ZeebeDeployment(classPathResources = "demoProcess.bpmn", bpmnLocations = "classpath*:/bpmn/**/*.bpmn")
+@ZeebeDeployment(classPathResources = "demoProcess.bpmn")
 @Slf4j
 public class StarterApplication {
 
